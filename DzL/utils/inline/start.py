49188ff965d1a248e7c9a7ca504c52050bae7bea
ALEXANDER <107063122+Alex11011@users.noreply.github.com>
2023-05-27 10:20:47 +0530

    Add files via upload

--- DzL/utils/inline/start.py
+++ DzL/utils/inline/start.py
@@ -1,28 +1,32 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from typing import Union
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton
 
-import config
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from DzL import app
 
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="ʜᴇʟᴩ",
-                callback_data="settings_back_helper",
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?start=help",
             ),
             InlineKeyboardButton(
-                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+                text=_["S_B_2"], callback_data="settings_helper"
             ),
         ],
-     ]
+    ]
     return buttons
 
 
@@ -30,34 +34,44 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
-        ],
-      [
-        InlineKeyboardButton(
-            text="🫧𝐂𝐇𝐀𝐍𝐍𝐄𝐋🫧",
-            url=f"https://t.me/ab_krishna_uff",
-        ) 
-     ],
-
-  [
-            InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
-            )
-        ],
+        ]
+    ]
+    buttons.append(
         [
             InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
+                text=_["S_B_8"], callback_data="settings_back_helper"
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                "Inline", switch_inline_query_current_chat=""
+            ),
+        ]
+    )
+    if GITHUB_REPO and OWNER:
+        buttons.append(
+            [
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(
+                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                ),
+            ]
+        )
+    else:
+        if GITHUB_REPO:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                    ),
+                ]
             )
-        ],
-[
-            InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=f"https://t.me/Ajanabee_Duniya"
-            )
-        ],
-     ]
+    buttons.append(
+            [
+                InlineKeyboardButton(
+                text=_["ST_B_6"], callback_data="LG"
+                ),
+            ]
+        )
     return buttons
